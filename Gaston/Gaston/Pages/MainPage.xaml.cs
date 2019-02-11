f272ff8d87be4bb44918ede2ab14629c7a91cb76
Campbell Louis (Macbook) <campbell.earl.louis@xanlosh.us>
2019-02-11 17:59:34 +0100

    Initial Commit

---
+++ Gaston/Gaston/Pages/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Gaston
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Settings_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new SettingsPage());
+        }
+    }
+}
